d21de77e7a612243986cdfd72315179e86d3a98f
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-31 05:35:25 +0000

    Binary Search Tree Creation

---
+++ BST_Desscription.cs
@@ -0,0 +1,10 @@
+﻿//Binary Search Tree is a node-based binary tree data structure which has the following properties:  
+
+//The left subtree of a node contains only nodes with keys lesser than the node’s key.
+//The right subtree of a node contains only nodes with keys greater than the node’s key.
+//The left and right subtree each must also be a binary search tree. 
+//There must be no duplicate nodes.
+//Illustration to insert 
+//1.Start from the root.
+//2.Compare the inserting element with root, if less than root, then recurse for left, else recurse for right. 
+//3. After reaching the end, just insert that node at left(if less than current) else right.
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BSTDemo
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+            tree.insert(56);
+            tree.insert(30);
+            tree.insert(70);
+        
+
+            // Print inorder traversal of the BST
+            tree.inorder();
+        }
+    }
+
+}
